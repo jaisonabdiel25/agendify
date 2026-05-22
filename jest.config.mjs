011b591da8c9dd0cf4dbb7f4944d252282330855
Jaison Palacio <jaisonpalaciodev@gmail.com>
2026-05-22 17:24:17 -0500

    feat: add jest configuration file for testing setup

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,9 @@
-import type { Config } from "jest"
 import nextJest from "next/jest.js"
 
 const createJestConfig = nextJest({ dir: "./" })
 
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/src/tests/setup.ts"],
   moduleNameMapper: {
